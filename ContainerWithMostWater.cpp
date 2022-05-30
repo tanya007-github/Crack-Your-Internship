8ed1179a3eef1acc21c238d51b72f96b753d17e8
tanya007-github <86552981+tanya007-github@users.noreply.github.com>
2022-05-30 12:51:01 +0530

    Finding max area of container that can exist

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height){
+        int ans=INT_MIN;
+        int left=0, right=height.size()-1;
+        while(left<right){
+            int area=(min(height[left], height[right]))*(right-left);
+            ans=max(ans, area);
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return ans;
+    }
+};
