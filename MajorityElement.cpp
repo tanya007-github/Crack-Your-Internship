bb21f0aad842fa44fbf0fcb13b45e0da9c4780a6
tanya007-github <86552981+tanya007-github@users.noreply.github.com>
2022-06-04 19:33:23 +0530

    majority element is one that occur > n/2 times

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count=0;              //moore voting algo
+        int count=0;               //moore voting algo
         int ele=0;
         for(int i=0;i<nums.size();i++){
             if(count==0){
